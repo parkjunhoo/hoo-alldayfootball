70e528fa620a0f555d8d21a4b75101be62e7b89a
Hoo <56852562+parkjunhoo@users.noreply.github.com>
2023-05-24 19:40:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': 'alldseaycfreotockeytball',
-    'mongodbUri': 'mongodb+srv://alldayfootball:thereisallgroundlevel11@cluster0.aj3b1.mongodb.net/alldayfootball?retryWrites=true&w=majority'
+    'secret': 'ㅁㄴㅇㄹ',
+    'mongodbUri': 'ㅁㄴㅇㄹㄴㅁㅇㄹ'
 }
